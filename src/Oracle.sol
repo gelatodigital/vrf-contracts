37d4c4df70bf66f67d59c593c7e352691c5ad0ef
Alberto <albicento.ac@gmail.com>
2023-07-06 16:18:57 +0200

    feat: Added w3f interaction functions

--- src/Oracle.sol
+++ src/Oracle.sol
@@ -8,6 +8,8 @@ contract GelatoVRFOracle {
   uint256 latestRound;
   address operator;
 
+  event RequestBeacon(address callbackReceiver) anonymous;
+
   constructor(address _operator) {
     operator = _operator;
   }
@@ -15,5 +17,16 @@ contract GelatoVRFOracle {
   function addBeacon(uint256 round, uint256 beacon) external {
     if (msg.sender != operator) revert Errors.NotOperator();
     beaconOf[round] = beacon;
+    latestRound = round;
+
+    // TODO should this emit something? Check for conflicts with anon events
+  }
+
+  function requestBeacon() external {
+    emit RequestBeacon(address(0));
+  }
+
+  function requestBeaconCallback() external {
+    emit RequestBeacon(msg.sender);
   }
 }
