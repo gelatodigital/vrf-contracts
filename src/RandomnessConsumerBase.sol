b696a399355521421e43372dcbdcb73e71f2b87f
Louis Bettens <louis@bettens.info>
2023-07-08 23:02:41 +0200

    feat: Fisher-Yates

--- src/RandomnessConsumerBase.sol
+++ src/RandomnessConsumerBase.sol
@@ -3,6 +3,13 @@ pragma solidity ^0.8.19;
 
 import "src/Oracle.sol";
 
+struct ShuffleState {
+	// i iterates from max to 0
+	uint8 i;
+	// available is set for all the numbers that have not yet been emitted
+	uint256 set;
+}
+
 /** Provides a simple interface to manage a contract-internal PRNG and pull
  * random numbers from it.
 */
@@ -71,10 +78,30 @@ abstract contract RandomnessConsumerBase {
 		}
 	}
 
-	/** return a distinct, uniformly ditributed number less than max, and advance the internal PRNG.
+	/** return a distinct, uniformly ditributed number less than limit, and advance the internal PRNG.
 	 * Pre-condition: caller must ensure the internal PRNG is seeded
 	  */
-	function unsafeRandomUint224(uint224 max) internal returns (uint224) {
-		return uint224(unsafeRandomUint256()) % max;
+	function unsafeRandomUint224(uint224 limit) internal returns (uint224) {
+		return uint224(unsafeRandomUint256()) % limit;
+	}
+
+	/** prepare to emit all non-negative numbers no greater than max in a random order.
+	  */
+	function startShuffle(uint8 max) internal returns (ShuffleState memory state) {
+		require(max < 256);
+		return ShuffleState(max, ~(uint256(-1) << max));
+	}
+	/** emit a number that was not yet emitted by this shuffle.
+	  */
+	function shuffle(ShuffleState memory state) internal returns (uint8 k) {
+		k = uint8(randomUint224(uint224(state.i)+1));
+		for(uint8 j = 0; j < k; j++) {
+			while (state.set & (1 << j) == 0) {
+			j++;
+			k++;
+			}
+		}
+		state.set &= ~(1 << k);
+		state.i--;
 	}
 }
