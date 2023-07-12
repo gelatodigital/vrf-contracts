d7a4214563005b5f3e39220e229348c2844f83a8
Alberto <albicento.ac@gmail.com>
2023-07-12 11:44:58 +0200

    feat: Events now trigger on-chain callback

--- web3-functions/vrf/index.ts
+++ web3-functions/vrf/index.ts
@@ -1,6 +1,6 @@
 import shuffle from "lodash/shuffle";
 import { Log } from "@ethersproject/providers";
-import { ethers, Contract } from "ethers";
+import { Contract } from "ethers";
 
 import {
   Web3Function,
@@ -14,14 +14,15 @@ import {
   ChainOptions,
 } from "drand-client";
 
-const CALLBACK_ABI = [
-  "function fullfillRandomness(uint256 round, uint256 randomness) external",
-];
+// contract abis
+const INBOX_ABI = ["event RequestedRandomness(uint256 round, address callback)"]
+const CALLBACK_ABI = ["function fullfillRandomness(uint256 round, uint256 randomness) external"]
 
 // w3f constants
 const MAX_DEPTH = 700;
 const MAX_RANGE = 100; // limit range of events to comply with rpc providers
 const MAX_REQUESTS = 100; // limit number of requests on every execution to avoid hitting timeout
+
 // drand constants
 const CHAIN_HASH =
   "dbd506d6ef76e5f386f41c651dcb808c5bcbd75471cc4eafa3f4df7ad4e4c493"; // fastnet hash
@@ -60,15 +61,19 @@ async function fetchDrandResponse(options: ChainOptions) {
 
 Web3Function.onRun(async (context: Web3FunctionContext) => {
   const { userArgs, storage, multiChainProvider } = context;
-  const inbox = userArgs.inbox as string;
 
   const provider = multiChainProvider.default();
+
+  const inboxAddress = userArgs.inbox as string;
+  const inbox = new Contract(inboxAddress, INBOX_ABI, provider)
 
   const currentBlock = await provider.getBlockNumber();
   const lastBlockStr = await storage.get("lastBlockNumber");
   let lastBlock = lastBlockStr
     ? parseInt(lastBlockStr)
     : currentBlock - MAX_DEPTH;
+
+  const topics = [inbox.interface.getEventTopic("RequestedRandomness")];
 
   // Fetch recent logs in range of 100 blocks
   const logs: Log[] = [];
@@ -77,11 +82,10 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
     nbRequests++;
     const fromBlock = lastBlock + 1;
     const toBlock = Math.min(fromBlock + MAX_RANGE, currentBlock);
-    console.log(`Fetching log events from blocks ${fromBlock} to ${toBlock}`);
     try {
       const eventFilter = {
-        address: inbox,
-        topics: [],
+        address: inbox.address,
+        topics: topics,
         fromBlock,
         toBlock,
       };
@@ -96,31 +100,29 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
     }
   }
 
-  console.log(logs);
+  const callData = []
 
-  const callbackAddr = "0x100Fe89E27ED155C8098264b8E57c57f6249C653";
-  const callback = new Contract(callbackAddr, CALLBACK_ABI, provider);
+  // Parse retrieved events
+  console.log(`Matched ${logs.length} new events`);
+  for (const log of logs) {
+    const event = inbox.interface.parseLog(log);
+    const [round, callbackAddress] = event.args;
+    const callback = new Contract(callbackAddress, CALLBACK_ABI, provider)
+    callData.push({to: callbackAddress, data: callback.interface.encodeFunctionData("fullfillRandomness", [round, 123456789])})
+  }
 
-  const options = {
-    disableBeaconVerification: false, // `true` disables checking of signatures on beacons - faster but insecure!!!
-    noCache: false, // `true` disables caching when retrieving beacons for some providers
-    chainVerificationParams: { chainHash: CHAIN_HASH, publicKey: PUBLIC_KEY }, // these are optional, but recommended! They are compared for parity against the `/info` output of a given node
-  };
+  // const options = {
+    // disableBeaconVerification: false, // `true` disables checking of signatures on beacons - faster but insecure!!!
+    // noCache: false, // `true` disables caching when retrieving beacons for some providers
+    // chainVerificationParams: { chainHash: CHAIN_HASH, publicKey: PUBLIC_KEY }  // these are optional, but recommended! They are compared for parity against the `/info` output of a given node
+  // }
 
-  const { round, randomness } = await fetchDrandResponse(options);
-  console.log("W3F: round: ", round);
-  const randomWord = ethers.BigNumber.from(`0x${randomness}`);
+  // const { round, randomness } = await fetchDrandResponse(options);
+  // console.log("W3F: round: ", round)
+  // const randomWord = ethers.BigNumber.from(`0x${randomness}`);
 
   return {
     canExec: true,
-    callData: [
-      {
-        to: callback.address,
-        data: callback.interface.encodeFunctionData("fullfillRandomness", [
-          round,
-          randomWord,
-        ]),
-      },
-    ],
+    callData
   };
 });
