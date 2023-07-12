5d7a1458e5b7ad3be1dd26034aec40d39a8e8c04
Alberto <albicento.ac@gmail.com>
2023-07-12 12:49:57 +0200

    feat: Adding custom round fetching

--- web3-functions/vrf/index.ts
+++ web3-functions/vrf/index.ts
@@ -12,7 +12,7 @@ import {
   HttpChainClient,
   HttpCachingChain,
   ChainOptions,
-} from "drand-client";
+} from 'drand-client'
 
 // contract abis
 const INBOX_ABI = ["event RequestedRandomness(uint256 round, address callback)"]
@@ -24,12 +24,16 @@ const MAX_RANGE = 100; // limit range of events to comply with rpc providers
 const MAX_REQUESTS = 100; // limit number of requests on every execution to avoid hitting timeout
 
 // drand constants
-const CHAIN_HASH =
-  "dbd506d6ef76e5f386f41c651dcb808c5bcbd75471cc4eafa3f4df7ad4e4c493"; // fastnet hash
-const PUBLIC_KEY =
-  "a0b862a7527fee3a731bcb59280ab6abd62d5c0b6ea03dc4ddf6612fdfc9d01f01c31542541771903475eb1ec6615f8d0df0b8b6dce385811d6dcf8cbefb8759e5e616a3dfd054c928940766d9a5b9db91e3b697e5d70a975181e007f87fca5e";
+const CHAIN_HASH: string = 'dbd506d6ef76e5f386f41c651dcb808c5bcbd75471cc4eafa3f4df7ad4e4c493' // fastnet hash
+const PUBLIC_KEY: string = 'a0b862a7527fee3a731bcb59280ab6abd62d5c0b6ea03dc4ddf6612fdfc9d01f01c31542541771903475eb1ec6615f8d0df0b8b6dce385811d6dcf8cbefb8759e5e616a3dfd054c928940766d9a5b9db91e3b697e5d70a975181e007f87fca5e'
 
-async function fetchDrandResponse(options: ChainOptions) {
+const DRAND_OPTIONS: ChainOptions = {
+  disableBeaconVerification: false, 
+  noCache: false, 
+  chainVerificationParams: { chainHash: CHAIN_HASH, publicKey: PUBLIC_KEY } 
+}
+
+async function fetchDrandResponse(round?: number) {
   // sequentially try different endpoints, in shuffled order for load-balancing
   const urls = shuffle([
     // Protocol labs endpoints
@@ -46,12 +50,10 @@ async function fetchDrandResponse(options: ChainOptions) {
   const errors: Error[] = [];
   for (const url of urls) {
     console.log(`Trying ${url}...`);
-    const chain = new HttpCachingChain(`${url}/${CHAIN_HASH}`, options);
-    const client = new HttpChainClient(chain, options);
+    const chain = new HttpCachingChain(`${url}/${CHAIN_HASH}`, DRAND_OPTIONS)
+    const client = new HttpChainClient(chain, DRAND_OPTIONS)
     try {
-      const drandResponse = await fetchBeacon(client);
-      console.log(drandResponse);
-      return drandResponse;
+      return await fetchBeacon(client, round);
     } catch (err) {
       errors.push(err as Error);
     }
@@ -106,20 +108,17 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
   console.log(`Matched ${logs.length} new events`);
   for (const log of logs) {
     const event = inbox.interface.parseLog(log);
-    const [round, callbackAddress] = event.args;
+    const [roundRequested, callbackAddress] = event.args;
     const callback = new Contract(callbackAddress, CALLBACK_ABI, provider)
-    callData.push({to: callbackAddress, data: callback.interface.encodeFunctionData("fullfillRandomness", [round, 123456789])})
+    // const { round: roundReceived, randomness } = await fetchDrandResponse(roundRequested == 0 ? undefined : roundRequested);
+    // const encodedRandomness = ethers.BigNumber.from(`0x${randomness}`);
+    // assert roundReceived == roundRequested
+    // callData.push({to: callbackAddress, data: callback.interface.encodeFunctionData("fullfillRandomness", [roundReceived, encodedRandomness])})
+    callData.push({to: callbackAddress, data: callback.interface.encodeFunctionData("fullfillRandomness", [roundRequested, 123456789])})
   }
 
-  // const options = {
-    // disableBeaconVerification: false, // `true` disables checking of signatures on beacons - faster but insecure!!!
-    // noCache: false, // `true` disables caching when retrieving beacons for some providers
-    // chainVerificationParams: { chainHash: CHAIN_HASH, publicKey: PUBLIC_KEY }  // these are optional, but recommended! They are compared for parity against the `/info` output of a given node
-  // }
-
-  // const { round, randomness } = await fetchDrandResponse(options);
   // console.log("W3F: round: ", round)
-  // const randomWord = ethers.BigNumber.from(`0x${randomness}`);
+  // const randomWord = 
 
   return {
     canExec: true,
