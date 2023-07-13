6027428f5f2638d7a02c292516e200b3ba4080be
Louis Bettens <louis@bettens.info>
2023-07-13 11:23:48 +0200

    lint

--- test/index.test.ts
+++ test/index.test.ts
@@ -16,9 +16,9 @@ import {
 } from "drand-client";
 
 // drand constants
-const CHAIN_HASH: string =
+const CHAIN_HASH =
   "dbd506d6ef76e5f386f41c651dcb808c5bcbd75471cc4eafa3f4df7ad4e4c493"; // fastnet hash
-const PUBLIC_KEY: string =
+const PUBLIC_KEY =
   "a0b862a7527fee3a731bcb59280ab6abd62d5c0b6ea03dc4ddf6612fdfc9d01f01c31542541771903475eb1ec6615f8d0df0b8b6dce385811d6dcf8cbefb8759e5e616a3dfd054c928940766d9a5b9db91e3b697e5d70a975181e007f87fca5e";
 
 const DRAND_OPTIONS: ChainOptions = {
--- web3-functions/vrf/index.ts
+++ web3-functions/vrf/index.ts
@@ -29,9 +29,9 @@ const MAX_RANGE = 100; // limit range of events to comply with rpc providers
 const MAX_REQUESTS = 100; // limit number of requests on every execution to avoid hitting timeout
 
 // drand constants
-const FASTNET_CHAIN_HASH: string =
+const FASTNET_CHAIN_HASH =
   "dbd506d6ef76e5f386f41c651dcb808c5bcbd75471cc4eafa3f4df7ad4e4c493";
-const PUBLIC_KEY: string =
+const PUBLIC_KEY =
   "a0b862a7527fee3a731bcb59280ab6abd62d5c0b6ea03dc4ddf6612fdfc9d01f01c31542541771903475eb1ec6615f8d0df0b8b6dce385811d6dcf8cbefb8759e5e616a3dfd054c928940766d9a5b9db91e3b697e5d70a975181e007f87fca5e";
 
 const DRAND_OPTIONS: ChainOptions = {
