3b0d432eb20dbfaddbcfade6e5ce9c074a27271b
Louis Bettens <louis@bettens.info>
2023-07-31 22:32:27 +0200

    feat: add request method to MockConsumer

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+import "./Consumer.sol";
+
 contract GelatoVRFInbox {
     event RequestedRandomness(
         uint256 round,
-        address callback,
+        GelatoVRFConsumer callback,
         address indexed sender
     );
 
-    function requestRandomness(uint256 round, address callback) external {
+    function requestRandomness(
+        uint256 round,
+        GelatoVRFConsumer callback
+    ) external {
         emit RequestedRandomness(round, callback, msg.sender);
     }
 }
--- contracts/MockConsumer.sol
+++ contracts/MockConsumer.sol
@@ -1,9 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-contract MockVRFConsumer {
+import "./Inbox.sol";
+import "./Consumer.sol";
+
+contract MockVRFConsumer is GelatoVRFConsumer {
     mapping(uint256 => uint256) public beaconOf;
     uint256 public latestRound;
+    GelatoVRFInbox public inbox;
+
+    constructor(GelatoVRFInbox _inbox) {
+        inbox = _inbox;
+    }
+
+    function requestRandomness(uint256 round) external {
+        inbox.requestRandomness(round, this);
+    }
 
     function fullfillRandomness(uint256 _round, uint256 _randomness) external {
         beaconOf[_round] = _randomness;
