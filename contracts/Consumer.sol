92bdb802266a20dc731e100e97f908b88cf98fe7
Louis Bettens <louis@bettens.info>
2023-08-02 18:56:21 +0200

    feat: use the latest Drand round

--- contracts/Consumer.sol
+++ contracts/Consumer.sol
@@ -2,5 +2,5 @@
 pragma solidity 0.8.18;
 
 interface GelatoVRFConsumer {
-    function fullfillRandomness(uint256 round, uint256 randomness) external;
+    function fullfillRandomness(uint256 randomness) external;
 }
--- contracts/MockConsumer.sol
+++ contracts/MockConsumer.sol
@@ -2,11 +2,9 @@
 pragma solidity 0.8.18;
 
 contract MockVRFConsumer {
-    mapping(uint256 => uint256) public beaconOf;
-    uint256 public latestRound;
+    uint256 public latestRandomness;
 
-    function fullfillRandomness(uint256 _round, uint256 _randomness) external {
-        beaconOf[_round] = _randomness;
-        latestRound = _round;
+    function fullfillRandomness(uint256 _randomness) external {
+        latestRandomness = _randomness;
     }
 }
