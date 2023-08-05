f6cf64686bf41c47279aa6da5af939f0ff8f3d81
U <nils.del@protonmail.com>
2023-08-05 12:12:43 +0200

    feat: factory emit events for deployment via frontend

--- contracts/chainlink_compatible/VRFCoordinatorV2AdapterFactory.sol
+++ contracts/chainlink_compatible/VRFCoordinatorV2AdapterFactory.sol
@@ -4,9 +4,13 @@ pragma solidity 0.8.18;
 import {VRFCoordinatorV2Adapter} from "./VRFCoordinatorV2Adapter.sol";
 
 contract VRFCoordinatorV2AdapterFactory {
+    event AdapterCreated(address indexed creator, address adapter);
+
     function make(
         address operator
     ) external returns (VRFCoordinatorV2Adapter adapter) {
-        return new VRFCoordinatorV2Adapter(operator);
+        adapter = new VRFCoordinatorV2Adapter(operator);
+        emit AdapterCreated(msg.sender, address(adapter));
+        return adapter;
     }
 }
