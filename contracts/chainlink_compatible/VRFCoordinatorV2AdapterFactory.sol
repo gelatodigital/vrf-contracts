b8266219a56de5a0f75f6f521792f680f95ecd18
Alberto <albicento.ac@gmail.com>
2023-08-07 11:10:53 +0200

    docs: Added natspec

--- contracts/chainlink_compatible/VRFCoordinatorV2AdapterFactory.sol
+++ contracts/chainlink_compatible/VRFCoordinatorV2AdapterFactory.sol
@@ -3,9 +3,18 @@ pragma solidity 0.8.18;
 
 import {VRFCoordinatorV2Adapter} from "./VRFCoordinatorV2Adapter.sol";
 
+/// @title VRFCoordinatorV2AdapterFactory
+/// @dev Factory contract for creating instances of VRFCoordinatorV2Adapter.
 contract VRFCoordinatorV2AdapterFactory {
+    /// @notice Event emitted when a new VRFCoordinatorV2Adapter is created.
+    /// @param creator The address that created the adapter.
+    /// @param adapter The address of the created adapter.
     event AdapterCreated(address indexed creator, address adapter);
 
+    /// @notice Create a new instance of VRFCoordinatorV2Adapter.
+    /// @dev Creates a new VRFCoordinatorV2Adapter contract with the provided operator address.
+    /// @param operator The address of the operator that will interact with the adapter.
+    /// @return adapter The newly created VRFCoordinatorV2Adapter instance.
     function make(
         address operator
     ) external returns (VRFCoordinatorV2Adapter adapter) {
@@ -13,3 +22,4 @@ contract VRFCoordinatorV2AdapterFactory {
         emit AdapterCreated(msg.sender, address(adapter));
     }
 }
+
