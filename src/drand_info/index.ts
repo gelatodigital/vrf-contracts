d9c555e5e2ff3a977647aa17e262a95dc575cb30
Louis Bettens <louis@bettens.info>
2023-08-27 14:54:08 +0200

    chore: remove dead code

--- src/drand_info/index.ts
+++ src/drand_info/index.ts
@@ -1,6 +1,4 @@
 import { ChainInfo } from "drand-client";
-import * as _fastnet from "./assets/fastnet.json" assert { type: "json" };
 import * as _quicknet from "./assets/quicknet.json" assert { type: "json" };
 
-export const fastnet: ChainInfo = _fastnet.default;
 export const quicknet: ChainInfo = _quicknet.default;
