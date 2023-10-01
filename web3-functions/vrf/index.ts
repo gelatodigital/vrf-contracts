5b292d8f6eb653bea8048a433d11d7ceacec5bc0
U <nils.del@protonmail.com>
2023-10-02 00:15:17 +0200

    feat: rate limiting and storage for upcoming transactions

--- web3-functions/vrf/index.ts
+++ web3-functions/vrf/index.ts
@@ -20,6 +20,7 @@ const MAX_RANGE = 100; // limit range of events to comply with rpc providers
 const MAX_REQUESTS = 5; // limit number of requests on every execution to avoid hitting timeout
 const MAX_DEPTH = MAX_RANGE * MAX_REQUESTS; // How far the VRF should catch up with blocks
 const MAX_DISTANCE = 1000; // Helpful to detect if the VRF has been paused not to recover too many blocks
+const MAX_TRANSACTIONS = 10; // limit number of transactions to avoid failure due to block gas limit
 
 Web3Function.onRun(async (context: Web3FunctionContext) => {
   const { userArgs, storage, multiChainProvider } = context;
@@ -64,25 +65,44 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
     }
   }
 
-  const callData = [];
-
   // Parse retrieved events
   console.log(`Matched ${logs.length} new events`);
   const randomness = await getNextRandomness();
   const encodedRandomness = ethers.BigNumber.from(`0x${randomness}`);
-  for (const log of logs) {
+
+  // Prepare calldata and rate limit
+  const oldCallDataStr = await storage.get("oldCallData");
+  const callData = oldCallDataStr ? JSON.parse(oldCallDataStr) : [];
+  const nextCallData = [];
+  const remainTx = MAX_TRANSACTIONS - callData.length
+
+  for (let i = 0; i < logs.length; i++) {
+    const log = logs[i];
     const event = consumer.interface.parseLog(log);
     const [data] = event.args;
-    callData.push({
-      to: consumerAddress,
-      data: consumer.interface.encodeFunctionData("fulfillRandomness", [
-        encodedRandomness,
-        data,
-      ]),
-    });
+  
+    if (i < remainTx) {
+      callData.push({
+        to: consumerAddress,
+        data: consumer.interface.encodeFunctionData("fulfillRandomness", [
+          encodedRandomness,
+          data,
+        ]),
+      });
+    } else {
+      nextCallData.push({
+        to: consumerAddress,
+        data: consumer.interface.encodeFunctionData("fulfillRandomness", [
+          encodedRandomness,
+          data,
+        ]),
+      });
+    }
   }
 
   await storage.set("lastBlockNumber", `${currentBlock}`);
+  await storage.set("oldCallData", JSON.stringify(nextCallData));
+
 
   return {
     canExec: true,
